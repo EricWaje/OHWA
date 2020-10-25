6846bd46f1223028865d9ec6aa22167b9a47e897
Eric Wajnrajch <ericwaje03@gmail.com>
2020-10-25 19:04:18 -0300

    cambios

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const nav = document.getElementById('barraNav');
+
+window.onscroll = function(menu){
+    if (window.pageYOffset > 120) {
+        nav.style.boxShadow = "0px 2px 15px -6px rgba(0,0,0,0.35)";
+        nav.style.transition = "all 0.2s ease"
+        nav.style.background = "white"
+        
+    }else{
+        nav.style.boxShadow = "0px 0px 0px rgb(0,0,0)";
+        nav.style.transition = "all 0.2s ease"
+        nav.style.background = "transparent"
+    }
+}
+menu();
+
+
