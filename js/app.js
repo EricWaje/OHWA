656d58ee6dfded56a1ab81df21e22d3a8d6981ee
Eric Wajnrajch <ericwaje03@gmail.com>
2020-10-26 11:51:59 -0300

    js cambios

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ window.onscroll = function(menu){
         nav.style.boxShadow = "0px 2px 15px -6px rgba(0,0,0,0.35)";
         nav.style.transition = "all 0.2s ease";
         nav.style.background = "white";
-        nav.style.height = "75px";
         
     }else{
         nav.style.boxShadow = "0px 0px 0px rgb(0,0,0)";
