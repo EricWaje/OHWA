bb8abef168593392ee9a441f7b569f2f3832ee68
Eric Wajnrajch <ericwaje03@gmail.com>
2020-12-12 14:52:50 -0300

    cambios

--- js/app.js
+++ js/app.js
@@ -1,14 +1,28 @@
 const nav = document.getElementById('barraNav');
 
-window.onscroll = function(){
-    if (window.pageYOffset > 120) {
-        nav.style.boxShadow = "0px 2px 15px -6px rgba(0,0,0,0.35)";
-        nav.style.transition = "all 0.2s ease";
-        nav.style.background = "white";
-        
-    }else{
-        nav.style.boxShadow = "0px 0px 0px rgb(0,0,0)";
-        nav.style.transition = "all 0.2s ease";
-        nav.style.background = "transparent";
-    }
+window.onscroll = function () {
+  if (window.pageYOffset > 120) {
+    nav.style.boxShadow = '0px 2px 15px -6px rgba(0,0,0,0.35)';
+    nav.style.transition = 'all 0.2s ease';
+    nav.style.background = 'white';
+  } else {
+    nav.style.boxShadow = '0px 0px 0px rgb(0,0,0)';
+    nav.style.transition = 'all 0.2s ease';
+    nav.style.background = 'transparent';
+  }
 };
+
+/* const cambioColor = () => {
+  const btnPlacas = document.getElementsByClassName('boton-placas');
+  btnPlacas.style.color = 'blue';
+};
+
+const start = () => {
+  document
+    .getElementsByClassName('boton-placas')
+    .addEventListener('click', () => cambioColor());
+  return btnPlacas;
+};
+
+window.onload = start;
+ */
